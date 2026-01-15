84302a7359b77f73b07512f42789ba726ffff5f2
Tonys <info@tony-schiller.com>
2026-01-15 16:09:38 +0100

    Fix docstring formatting and improve comments in Cart class

--- cart/cart.py
+++ cart/cart.py
@@ -9,12 +9,14 @@ class Cart:
         Initialize the cart.
 
         Args:
-            request (_type_): session cookie
+            request (_type_): Session cookie
         """
-        self.session = request.session
-        cart = self.session.get(settings.CART_SESSION_ID)
+        self.session = request.session  # store current session
+        cart = self.session.get(
+            settings.CART_SESSION_ID
+        )  # get cart from current session, if not available create empty
         if not cart:
             # save empty cart in the session
             cart = self.session[settings.CART_SESSION_ID] = {}
 
-        self.cart = cart
+        self.cart = cart  # create cart dictionary
