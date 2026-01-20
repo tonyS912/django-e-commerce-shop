e935b043d0b6314399deb841296298cf0070e772
Tonys <info@tony-schiller.com>
2026-01-20 10:44:15 +0100

    Fix syntax error in cart_add function by correcting cleaned_data assignment

--- cart/views.py
+++ cart/views.py
@@ -11,7 +11,8 @@ def cart_add(request, product_id):
     product = get_object_or_404(Product, id=product_id)
     form = CartAddProductForm(request.POST)
     if form.is_valid():
-        cd = form.cleaned_datacar.add(
+        cd = form.cleaned_data
+        cart.add(
             product=product, quantity=cd["quantity"], override_quantity=cd["override"]
         )
     return redirect("cart:cart_detail")
