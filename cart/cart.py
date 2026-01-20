74f1fe41affaec3bb116a61c30d0efc8254bae65
Tonys <info@tony-schiller.com>
2026-01-20 10:53:25 +0100

    Fix typos in cart methods and improve iteration logic

--- cart/cart.py
+++ cart/cart.py
@@ -33,7 +33,7 @@ class Cart:
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id] = {"quantity": 0, "price": str(product.price)}
-        if override_quanitity:
+        if override_quantity:
             self.cart[product_id]["quantity"] = quantity
         else:
             self.cart[product_id]["quantity"] += quantity
@@ -55,7 +55,7 @@ class Cart:
             del self.cart[product_id]
             self.save()
 
-    def ___iter__(self):
+    def __iter__(self):
         """
         Iterate over the items in the cart and geht the products from the database.
         """
@@ -65,7 +65,7 @@ class Cart:
         cart = self.cart.copy()
         for product in products:
             cart[str(product.id)]["product"] = product
-        for item in cart.value():
+        for item in cart.values():
             item["price"] = Decimal(item["price"])
             item["total_price"] = item["price"] * item["quantity"]
             yield item
@@ -74,7 +74,7 @@ class Cart:
         """
         Count all items in the cart.
         """
-        return sum(item["quantity"] for item in self.cart.value())
+        return sum(item["quantity"] for item in self.cart.values())
 
     def get_total_price(self):
         return sum(
